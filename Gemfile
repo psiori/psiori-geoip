451dcd2aa4c76704c728443728bc0d12c6ab0718
salange <sascha@5dlab.com>
2014-11-10 23:28:27 +0100

    performance tests

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'dalli'
 gem 'pg',  group: :production
 gem 'thin'
 gem 'rails-perftest'
+gem 'ruby-prof'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ test/performance/api_geoip_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+require 'rails/performance_test_help'
+
+class ApiGeoipTest < ActionDispatch::PerformanceTest
+  # Refer to the documentation for all available options
+  # self.profile_options = { runs: 5, metrics: [:wall_time, :memory],
+  #                          output: 'tmp/performance', formats: [:flat] }
+
+  test "homepage" do
+    get '/api/v01/ip/87.128.57.12'
+  end
+end
